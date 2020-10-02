3fe0f20d51d39bc1c885309efcae24e451bfde13
荆启超 <1209446716@qq.com>
2020-10-03 02:01:53 +0800

    更新架包

--- jqc/jqc-sharding-jdbc/src/main/java/com/jqc/sharding/ShardingJdbcApplication.java
+++ jqc/jqc-sharding-jdbc/src/main/java/com/jqc/sharding/ShardingJdbcApplication.java
@@ -1,6 +1,11 @@
 package com.jqc.sharding;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ShardingJdbcApplication {
     public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcApplication.class, args);
     }
 }
