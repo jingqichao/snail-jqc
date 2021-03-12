a836541e5c553f60eb7015502235257ee2a0b764
jingqc <jingqc@yiche.com>
2021-03-12 10:00:00 +0800

    测试一下

--- jqc/jqc-registry/src/main/java/com/jqc/eureka/EurekaApplication.java
+++ jqc/jqc-registry/src/main/java/com/jqc/eureka/EurekaApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
 
+    //TODO 测试
     /**
      * 加载启动类,以启动类为当前springBoot的配置类标准
      * @param args
